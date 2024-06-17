be9b12aea8f4f18eeb88475fe10a85705b295653
Gustavo <gugacontas1303@gmail.com>
2024-06-17 20:24:13 -0300

    Finalizando Front-End

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from homepage.views import index,cachorros
 
 urlpatterns = [
-    path('', index),
-    path('cachorros/',cachorros, name='chachorros')
+    path('', index, name = 'home'),
+    path('cachorros/',cachorros, name='cachorros')
 ]
